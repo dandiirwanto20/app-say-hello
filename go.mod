a52fac9ec1d1696d507af0f38b3812390a38dd1e
Dandi Irwanto <dandi.ir20@gmail.com>
2024-03-17 14:58:59 +0700

    dev: app for learn go modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dandiirwanto20/app-say-hello
+
+go 1.20
+
+require github.com/dandiirwanto20/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/dandiirwanto20/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Dandi"))
+}
